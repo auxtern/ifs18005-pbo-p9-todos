b349320daa65796c43ef1b4e96d7d5d3c713b877
Abdullah Ubaid <Abdullahfak.au@gmail.com>
2025-11-05 06:52:13 +0700

    Update HomeController.java

--- src/main/java/org/delcom/todos/controllers/HomeController.java
+++ src/main/java/org/delcom/todos/controllers/HomeController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/") /*  */
+    // Endpoint untuk halaman utama
+    @GetMapping("/")
     public String hello() {
         return "Hay, selamat datang di Spring Boot!";
     }
